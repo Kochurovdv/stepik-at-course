3c7e3868a8f5a9a213a081bee5166c5f32b3f36a
Kochurovdv <scifiguy@yandex.ru>
2021-06-09 17:28:40 +0300

    homework 6 and 7 added (almost the same, different links)

---
+++ task6.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+import time 
+
+link = "http://suninjuly.github.io/registration1.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    input1 = browser.find_element_by_xpath('//div//div[@class="first_block"]//input[@class="form-control first"]')
+    input1.send_keys("Ivan")
+    input2 = browser.find_element_by_xpath('//div//div[@class="first_block"]//input[@class="form-control second"]')
+    input2.send_keys("Petrov")
+    input3 = browser.find_element_by_xpath('//div//div[@class="first_block"]//input[@class="form-control third"]')
+    input3.send_keys("Smolensk@ya.ru")
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    time.sleep(30)
+    browser.quit()
